4b0d5914f19fb8fa7e30b11f4a5f75f1dbdd1f93
Loo Huai Yuan <loohuaiyuan@gmail.com>
2024-12-30 18:18:38 +0800

    fff

--- Project/src/LMS/Book.java
+++ Project/src/LMS/Book.java
@@ -36,7 +36,25 @@ public class Book {
 
 
     // printing all hold req on a book.
-    
+    public void printHoldRequests()
+    {
+        if (!holdRequestsOperations.holdRequests.isEmpty())
+        { 
+            System.out.println("\nHold Requests are: ");
+            
+            System.out.println("---------------------------------------------------------------------------------------------------------------------------------------");            
+            System.out.println("No.\t\tBook's Title\t\t\tBorrower's Name\t\t\tRequest Date");
+            System.out.println("---------------------------------------------------------------------------------------------------------------------------------------");
+            
+            for (int i = 0; i < holdRequestsOperations.holdRequests.size(); i++)
+            {                      
+                System.out.print(i + "-" + "\t\t");
+                holdRequestsOperations.holdRequests.get(i).print();
+            }
+        }
+        else
+            System.out.println("\nNo Hold Requests.");                                
+    }
     
     // printing book's Info
     public void printInfo()
@@ -44,20 +62,7 @@ public class Book {
         System.out.println(title + "\t\t\t" + author + "\t\t\t" + subject);
     }
     
-    // changign Info of a Book
-    public void changeBookInfo() throws IOException
-    {
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
-        System.out.println("\nUpdate Author? (y/n)");
-        input = scanner.next();
-        
-        System.out.println("\nBook is successfully updated.");
-        
-    }
+    
     
     /*------------Getter FUNCs.---------*/
     
