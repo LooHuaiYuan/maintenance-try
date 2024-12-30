af827f522facda1a66a79fd3bf241ffa8de8792f
User <215382@student.upm.edu.my>
2024-12-30 17:23:32 +0800

    halo loo

--- Project/src/LMS/Book.java
+++ Project/src/LMS/Book.java
@@ -81,21 +81,7 @@ public class Book {
 
         System.out.println("\nUpdate Subject? (y/n)");
         input = scanner.next();
-        
-        if(input.equals("y"))
-        {
-            System.out.println("\nEnter new Subject: ");
-            subject = reader.readLine();
-        }
-
-        System.out.println("\nUpdate Title? (y/n)");
-        input = scanner.next();
-        
-        if(input.equals("y"))
-        {
-            System.out.println("\nEnter new Title: ");
-            title = reader.readLine();
-        }        
+             
         
         System.out.println("\nBook is successfully updated.");
         
